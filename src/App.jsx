8e8c03bfa3d94db1bc5c390d4d17da7b706776a9
wasyn2025 <wasynssulaiman@gmail.com>
2026-08-05 11:05:30 +0700

    Added restart button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Earth, AlarmClock } from 'lucide-react';
+import { Earth, AlarmClock, RotateCcw, Keyboard } from 'lucide-react';
 import Word from './components/Word.jsx';
 
 const wordList = [
@@ -23,24 +23,33 @@ function App() {
 
   return (
     <div className='w-5/6 mx-auto h-full flex flex-col gap-y-40'>
-      <div className='flex justify-between items-center dm-san'>
-        <h1 className='text-3xl text-shade-white font-medium'>keyboardtype</h1>
-        <span className='flex items-center gap-2 text-shade-white text-sm cursor-pointer py-1 px-2 rounded-md hover:bg-white/10'>
-          <Earth size={18} />
-          Indonesia
-        </span>
+      <div className='flex items-center dm-sans text-shade-white gap-2'>
+        <Keyboard size={34} />
+        <h1 className='text-3xl font-medium'>keyboardtype</h1>
       </div>
 
       <div className='grow text-shade-white'>
         <div className='w-full'>
-          <p className='text-3xl font-medium mb-5 opacity-0'>30</p>
-          <div className='select-none text-[2.5rem] leading-none crimson-pro flex flex-wrap gap-x-3 gap-y-2 h-[9rem] overflow-hidden'>
+
+          <div className='w-full relative mb-3'>
+            <p className='absolute top-0 left-0 opacity-0 text-3xl font-medium dm-sans'>30</p>
+            <span className='w-fit mx-auto flex items-center dm-sans gap-2 text-shade-white text-sm cursor-pointer py-1 px-2 rounded-md hover:bg-white/10'>
+              <Earth size={18} />
+              Indonesia
+            </span>
+          </div>
+
+          <div className='mb-3 select-none text-[2.5rem] leading-none crimson-pro flex flex-wrap gap-x-3 gap-y-3 h-[9.5rem] overflow-hidden'>
             {
               words.map((word, index) => (
                 <Word key={index} text={word} wordIndex={index} />
               ))
             }
           </div>
+
+          <button className='w-fit block cursor-pointer mx-auto p-1 rounded-md hover:bg-white/10'>
+             <RotateCcw size={22} />
+          </button>
         </div>
       </div>
     </div>
