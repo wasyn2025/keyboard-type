6787b77b6bb01956207060c73db4f675a40c2f07
wasyn2025 <wasynssulaiman@gmail.com>
2026-08-12 12:04:52 +0700

    Fix caret top position

--- src/components/Caret.jsx
+++ src/components/Caret.jsx
@@ -1,9 +1,9 @@
 export default function Caret({isFocus, caretPosition}) {
     return (
         <div
-            className={`absolute w-0.75 h-11 bg-(--caret-color) transition-[top,left] duration-100 ease-linear ${isFocus ? '' : 'caret-blink'}`}
+            className={`absolute w-0.75 h-10 bg-(--caret-color) transition-[top,left] duration-100 ease-linear ${isFocus ? '' : 'caret-blink'}`}
             style={{
-                top: `${caretPosition.top}px`,
+                top: `${caretPosition.top + 6}px`,
                 left: `${caretPosition.left}px`
             }}
         />
