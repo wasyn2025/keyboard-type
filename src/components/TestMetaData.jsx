3000af43521db2b7b3f1582b90970e88efcd242b
wasyn2025 <wasynssulaiman@gmail.com>
2026-08-19 10:56:02 +0700

    Remove unuse components file

--- src/components/TestMetaData.jsx
+++
@@ -1,5 +0,0 @@
-export default function TestMetaData({ text }) {
-    return (
-        <p className='text-(--sub-color) text-base'>{text}</p>
-    );
-}
--- src/components/TestMetaDataContainer.jsx
+++
@@ -1,7 +0,0 @@
-export default function TestMetaDataContainer({ children }) {
-    return (
-        <div className='flex items-center justify-between'>
-            {children}
-        </div>
-    );
-}
