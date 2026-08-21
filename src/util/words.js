eca5192044ca1ab19d1ef7073639cb062f76fadd
wasyn2025 <wasynssulaiman@gmail.com>
2026-08-21 15:17:26 +0700

    Delete words.js
    words.js file is unuse and currently, keybordtpye uses random package for generating random words

--- src/util/words.js
+++
@@ -1,5 +0,0 @@
-export const wordList = [
-  "halo", "polisi", "laravel", "samsung", "tertawa",
-  "dunia", "rumah", "makanan", "coklat", "menari",
-  "programming", "coding", "kardus", "ungu", "jalan",
-];
